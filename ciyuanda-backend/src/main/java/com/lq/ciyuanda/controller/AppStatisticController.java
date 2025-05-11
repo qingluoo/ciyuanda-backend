8302ade4a2038fdb6e0e45952ca93ccfca9359e2
qingluoo <113030300+qingluoo@users.noreply.github.com>
2025-05-11 17:56:14 +0800

    修改补充

---
+++ ciyuanda-backend/src/main/java/com/lq/ciyuanda/controller/AppStatisticController.java
@@ -0,0 +1,51 @@
+package com.lq.ciyuanda.controller;
+
+import com.lq.ciyuanda.common.BaseResponse;
+import com.lq.ciyuanda.common.ErrorCode;
+import com.lq.ciyuanda.common.ResultUtils;
+import com.lq.ciyuanda.exception.ThrowUtils;
+import com.lq.ciyuanda.mapper.UserAnswerMapper;
+import com.lq.ciyuanda.model.dto.statistic.AppAnswerCountDTO;
+import com.lq.ciyuanda.model.dto.statistic.AppAnswerResultCountDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * App 统计分析接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/app/statistic")
+@Slf4j
+public class AppStatisticController {
+
+    @Resource
+    private UserAnswerMapper userAnswerMapper;
+
+    /**
+     * 热门应用及回答数统计（top 10）
+     *
+     * @return
+     */
+    @GetMapping("/answer_count")
+    public BaseResponse<List<AppAnswerCountDTO>> getAppAnswerCount() {
+        return ResultUtils.success(userAnswerMapper.doAppAnswerCount());
+    }
+
+    /**
+     * 某应用回答结果分布统计
+     *
+     * @param appId
+     * @return
+     */
+    @GetMapping("/answer_result_count")
+    public BaseResponse<List<AppAnswerResultCountDTO>> getAppAnswerResultCount(Long appId) {
+        ThrowUtils.throwIf(appId == null || appId <= 0, ErrorCode.PARAMS_ERROR);
+        return ResultUtils.success(userAnswerMapper.doAppAnswerResultCount(appId));
+    }
+}
